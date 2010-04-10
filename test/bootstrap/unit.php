e022689e03f423296ce80988f10125e688c1531e
Kris Wallsmith <kris.wallsmith@gmail.com>
2010-04-09 20:21:56 -0700

    Removed Lime, reorganized test directory.

--- test/bootstrap/unit.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require __DIR__.'/../lib/vendor/lime/LimeAutoloader.php';
-\LimeAutoloader::register();
-
-require __DIR__.'/../../lib/Buzz/ClassLoader.php';
-\Buzz\ClassLoader::register();
