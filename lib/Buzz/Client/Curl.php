73ff77a656dbdb38f83899771dee4ce70f087937
Kris Wallsmith <kris.wallsmith@gmail.com>
2010-10-26 17:31:00 -0700

    Added timeout and max-redirects to cURL client.

--- lib/Buzz/Client/Curl.php
+++ lib/Buzz/Client/Curl.php
@@ -7,6 +7,8 @@ use Buzz\Message;
 class Curl implements ClientInterface
 {
     protected $curl;
+    protected $maxRedirects = 0;
+    protected $timeout = 5;
 
     static protected function createCurlHandle()
     {
@@ -14,7 +16,6 @@ class Curl implements ClientInterface
 
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($curl, CURLOPT_HEADER, true);
-        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
 
         return $curl;
     }
@@ -37,9 +38,34 @@ class Curl implements ClientInterface
         return $this->curl;
     }
 
+    public function setMaxRedirects($maxRedirects)
+    {
+        $this->maxRedirects = $maxRedirects;
+    }
+
+    public function getMaxRedirects()
+    {
+        return $this->maxRedirects;
+    }
+
+    public function setTimeout($timeout)
+    {
+        $this->timeout = $timeout;
+    }
+
+    public function getTimeout()
+    {
+        return $this->timeout;
+    }
+
     public function send(Message\Request $request, Message\Response $response)
     {
         static::setCurlOptsFromRequest($this->curl, $request);
+
+        curl_setopt($this->curl, CURLOPT_TIMEOUT, $this->timeout);
+        curl_setopt($this->curl, CURLOPT_FOLLOWLOCATION, 0 < $this->maxRedirects);
+        curl_setopt($this->curl, CURLOPT_MAXREDIRS, $this->maxRedirects);
+
         $response->fromString(curl_exec($this->curl));
     }
 
