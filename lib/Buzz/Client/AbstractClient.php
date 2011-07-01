d5a2ce2554f7737b259b9d9e4fe26fe03ccceb91
Jean-François Simon <contact@jfsimon.fr>
2011-07-01 06:20:29 +0200

    removed duplicated method

--- lib/Buzz/Client/AbstractClient.php
+++ lib/Buzz/Client/AbstractClient.php
@@ -39,27 +39,4 @@ abstract class AbstractClient
     {
         return $this->timeout;
     }
-
-    /**
-     * Converts a request into an array for stream_context_create().
-     * 
-     * @param Message\Request $request A request object
-     * 
-     * @return array An array for stream_context_create()
-     */
-    public function getStreamContextArray(Message\Request $request)
-    {
-        return array('http' => array(
-            // values from the request
-            'method'           => $request->getMethod(),
-            'header'           => implode("\r\n", $request->getHeaders()),
-            'content'          => $request->getContent(),
-            'protocol_version' => $request->getProtocolVersion(),
-
-            // values from the current client
-            'ignore_errors'    => $this->getIgnoreErrors(),
-            'max_redirects'    => $this->getMaxRedirects(),
-            'timeout'          => $this->getTimeout(),
-        ));
-    }
 }
