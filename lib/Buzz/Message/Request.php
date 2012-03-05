d13b1a25c9d994f72e1a26e37446c0b0eb5da342
Konstantin Kudryashov <ever.zet@gmail.com>
2012-03-05 16:12:22 +0100

    Added `OPTIONS` method to the Request

--- lib/Buzz/Message/Request.php
+++ lib/Buzz/Message/Request.php
@@ -4,12 +4,13 @@ namespace Buzz\Message;
 
 class Request extends AbstractMessage implements RequestInterface
 {
-    const METHOD_GET    = 'GET';
-    const METHOD_HEAD   = 'HEAD';
-    const METHOD_POST   = 'POST';
-    const METHOD_PUT    = 'PUT';
-    const METHOD_DELETE = 'DELETE';
-    const METHOD_PATCH  = 'PATCH';
+    const METHOD_OPTIONS = 'OPTIONS';
+    const METHOD_GET     = 'GET';
+    const METHOD_HEAD    = 'HEAD';
+    const METHOD_POST    = 'POST';
+    const METHOD_PUT     = 'PUT';
+    const METHOD_DELETE  = 'DELETE';
+    const METHOD_PATCH   = 'PATCH';
 
     private $method;
     private $resource;
