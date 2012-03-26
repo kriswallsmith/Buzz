c2d2bf3885a95caa82304afd09ab291c0a94dd65
Kris Wallsmith <kris.wallsmith@gmail.com>
2012-03-26 06:02:31 -0700

    added docblocks to message interfaces

--- lib/Buzz/Message/MessageInterface.php
+++ lib/Buzz/Message/MessageInterface.php
@@ -2,9 +2,31 @@
 
 namespace Buzz\Message;
 
+/**
+ * An HTTP message.
+ *
+ * @author Kris Wallsmith <kris.wallsmith@gmail.com>
+ */
 interface MessageInterface
 {
+    /**
+     * Returns an array of header lines.
+     *
+     * @return array An array of headers (integer indexes)
+     */
     function getHeaders();
+
+    /**
+     * Adds a header to this message.
+     *
+     * @param string $header A header line
+     */
     function addHeader($header);
+
+    /**
+     * Returns the content of the message.
+     *
+     * @return string The message content
+     */
     function getContent();
 }
--- lib/Buzz/Message/RequestInterface.php
+++ lib/Buzz/Message/RequestInterface.php
@@ -2,12 +2,52 @@
 
 namespace Buzz\Message;
 
+/**
+ * An HTTP request message.
+ *
+ * @author Kris Wallsmith <kris.wallsmith@gmail.com>
+ */
 interface RequestInterface extends MessageInterface
 {
+    /**
+     * Returns the HTTP method of the current request.
+     *
+     * @return string An HTTP method
+     */
     function getMethod();
+
+    /**
+     * Returns the resource portion of the request line.
+     *
+     * @return string The resource requested
+     */
     function getResource();
+
+    /**
+     * Returns the protocol version of the current request.
+     *
+     * @return float The protocol version
+     */
     function getProtocolVersion();
+
+    /**
+     * Returns the value of the host header.
+     *
+     * @return string|null The host
+     */
     function getHost();
+
+    /**
+     * Returns the URL of the current request.
+     *
+     * @return string As full an URL as possible
+     */
     function getUrl();
+
+    /**
+     * Checks if the current request is secure.
+     *
+     * @return Boolean True if the request is secure
+     */
     function isSecure();
 }
