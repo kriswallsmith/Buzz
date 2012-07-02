5010d0cdad876969213f719c7f21c24bd9fdf65c
Johanna Cherry <johanna@hailocab.com>
2012-07-02 12:09:55 +0100

    Do not change error reporting level as it doesn't affect they intended behaviour of throwing the exception.

--- lib/Buzz/Client/FileGetContents.php
+++ lib/Buzz/Client/FileGetContents.php
@@ -42,9 +42,7 @@ class FileGetContents extends AbstractStream implements ClientInterface
         $context = stream_context_create($this->getStreamContextArray($request));
         $url = $request->getHost().$request->getResource();
 
-        $level = error_reporting(0);
         $content = file_get_contents($url, 0, $context);
-        error_reporting($level);
         if (false === $content) {
             $error = error_get_last();
             throw new \RuntimeException($error['message']);
