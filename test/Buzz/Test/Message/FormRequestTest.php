fac9e10f506115daef5d599fd3235f1c3af7e9da
trta <honza.trtik@gmail.com>
2014-09-04 10:27:54 +0200

    Buzz\Message\Form\FormRequest::getContent() - fixed http_build_query dependency on arg_separator.output setting - separator is always & when content type is application/x-www-form-urlencoded

--- test/Buzz/Test/Message/FormRequestTest.php
+++ test/Buzz/Test/Message/FormRequestTest.php
@@ -22,7 +22,24 @@ class FormRequestTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals($expected, $message->getContent());
     }
 
-    public function testAddDataAddsData()
+	public function testGetContentGeneratesContentWithCustomArgSeparatorOutput()
+	{
+
+		$argSeparatorOutput = ini_get('arg_separator.output');
+		ini_set('arg_separator.output', '&amp;');
+
+		$message = new FormRequest();
+		$message->setField('foo', 'bar');
+		$message->setField('bar', 'foo');
+
+		$expected = "foo=bar&bar=foo";
+		$this->assertEquals($expected, $message->getContent());
+
+		ini_set('arg_separator.output', $argSeparatorOutput);
+	}
+
+
+	public function testAddDataAddsData()
     {
         $message = new FormRequest();
         $message->setField('foo', 'bar');
