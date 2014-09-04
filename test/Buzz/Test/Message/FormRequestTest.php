3d56a7ed28143d28b31f1357561b5ed66c9d1f10
trta <honza.trtik@gmail.com>
2014-09-04 12:08:12 +0200

    tabs -> spaces

--- test/Buzz/Test/Message/FormRequestTest.php
+++ test/Buzz/Test/Message/FormRequestTest.php
@@ -22,24 +22,24 @@ class FormRequestTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals($expected, $message->getContent());
     }
 
-	public function testGetContentGeneratesContentWithCustomArgSeparatorOutput()
-	{
+    public function testGetContentGeneratesContentWithCustomArgSeparatorOutput()
+    {
 
-		$argSeparatorOutput = ini_get('arg_separator.output');
-		ini_set('arg_separator.output', '&amp;');
+        $argSeparatorOutput = ini_get('arg_separator.output');
+        ini_set('arg_separator.output', '&amp;');
 
-		$message = new FormRequest();
-		$message->setField('foo', 'bar');
-		$message->setField('bar', 'foo');
+        $message = new FormRequest();
+        $message->setField('foo', 'bar');
+        $message->setField('bar', 'foo');
 
-		$expected = "foo=bar&bar=foo";
-		$this->assertEquals($expected, $message->getContent());
+        $expected = "foo=bar&bar=foo";
+        $this->assertEquals($expected, $message->getContent());
 
-		ini_set('arg_separator.output', $argSeparatorOutput);
-	}
+        ini_set('arg_separator.output', $argSeparatorOutput);
+    }
 
 
-	public function testAddDataAddsData()
+    public function testAddDataAddsData()
     {
         $message = new FormRequest();
         $message->setField('foo', 'bar');
