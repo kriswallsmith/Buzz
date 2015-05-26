d3e559ce660b6a9fc545768fbe2393ca11b2b9fe
Simon Verraest <simon@siverra.be>
2015-05-26 12:32:26 +0100

    Add verify_host option

--- lib/Buzz/Client/AbstractCurl.php
+++ lib/Buzz/Client/AbstractCurl.php
@@ -224,6 +224,7 @@ abstract class AbstractCurl extends AbstractClient
         curl_setopt($curl, CURLOPT_MAXREDIRS, $canFollow ? $this->getMaxRedirects() : 0);
         curl_setopt($curl, CURLOPT_FAILONERROR, !$this->getIgnoreErrors());
         curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, $this->getVerifyPeer());
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, $this->getVerifyHost());
 
         // apply additional options
         curl_setopt_array($curl, $options + $this->options);
