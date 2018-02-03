89e2fa1fd6654feeac9a52a171222f97309eb955
Nyholm <tobias.nyholm@gmail.com>
2018-02-03 23:47:56 +0100

    Adding request builder for form submit

---
+++ lib/Buzz/Message/FormRequestBuilder.php
@@ -0,0 +1,76 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Buzz\Message;
+
+class FormRequestBuilder
+{
+    /**
+     * @var array
+     */
+    private $data;
+
+    /**
+     * @var array
+     */
+    private $files;
+
+    /**
+     *
+     * @param array $data
+     * @param array $files
+     */
+    public function __construct(array $data = [], array $files = [])
+    {
+        $this->data = $data;
+        $this->files = $files;
+    }
+
+    /**
+     * @param string $name
+     * @param string $value
+     */
+    public function addField($name, $value)
+    {
+        $this->data[$name] = $value;
+    }
+
+    /**
+     * @param string $name
+     * @param string $path
+     * @param string|null $contentType
+     * @param string|null $filename
+     */
+    public function addFile($name, $path, $contentType = null, $filename = null)
+    {
+        $this->files[$name] = [
+            'path' => $path,
+            'contentType' => $contentType,
+            'filename' => $filename,
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function build()
+    {
+        $data = $this->data;
+
+        foreach ($this->files as $name => $file) {
+            $data[$name] = $file;
+        }
+
+        return $data;
+    }
+
+    /**
+     * Reset the builder
+     */
+    public function reset()
+    {
+        $this->data = [];
+        $this->files = [];
+    }
+}
