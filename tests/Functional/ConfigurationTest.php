7eb6c4e1d5ba18bd0dc9764f997dad22656a2f33
Nyholm <tobias.nyholm@gmail.com>
2018-08-02 22:04:17 +0200

    Fixed some tests

--- tests/Functional/ConfigurationTest.php
+++ tests/Functional/ConfigurationTest.php
@@ -42,7 +42,7 @@ class ConfigurationTest extends TestCase
      */
     public function testOptionInConstructor($class)
     {
-        $client = new $class(['timeout' => 4], new Psr17Factory());
+        $client = new $class(new Psr17Factory(), ['timeout' => 4]);
         $this->assertInstanceOf($class, $client);
     }
 
@@ -55,7 +55,7 @@ class ConfigurationTest extends TestCase
             $this->markTestSkipped('The test server is not configured.');
         }
 
-        $client = new $class([], new Psr17Factory());
+        $client = new $class(new Psr17Factory(), []);
 
         $response = $client->sendRequest(new Request('GET', $_SERVER['BUZZ_TEST_SERVER']), ['timeout' => 4]);
         $this->assertInstanceOf(ResponseInterface::class, $response);
