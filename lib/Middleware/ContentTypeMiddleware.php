e4f8a42beb8fc3c252edc8f3c7c3e6f550fe27de
Giuliano Pietoso <giuliano.pietoso@tradeforce.com>
2018-08-04 16:53:05 -0300

    Implement Content-Type middleware (#333)
    * Implement ContentType middleware
    
    * Refactors validations
    
    * Further refactors validations
    
    * Fixing 'typo'
    
    * Runs PHP-CS on test file and middleware file.
    
    * PHP-CS fix

---
+++ lib/Middleware/ContentTypeMiddleware.php
@@ -0,0 +1,114 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Buzz\Middleware;
+
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\StreamInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ContentTypeMiddleware implements MiddlewareInterface
+{
+    /**
+     * Allow to disable the content type detection when stream is too large (as it can consume a lot of resource).
+     *
+     * @var bool
+     *
+     * true     skip the content type detection
+     * false    detect the content type (default value)
+     */
+    protected $skipDetection;
+
+    /**
+     * Determine the size stream limit for which the detection as to be skipped (default to 16Mb).
+     *
+     * @var int
+     */
+    protected $sizeLimit;
+
+    /**
+     * @param array $config {
+     *
+     *     @var bool $skip_detection True skip detection if stream size is bigger than $size_limit
+     *     @var int $size_limit size stream limit for which the detection as to be skipped.
+     * }
+     */
+    public function __construct(array $config = [])
+    {
+        $resolver = new OptionsResolver();
+        $resolver->setDefaults([
+            'skip_detection' => false,
+            'size_limit' => 16000000,
+        ]);
+        $resolver->setAllowedTypes('skip_detection', 'bool');
+        $resolver->setAllowedTypes('size_limit', 'int');
+        $options = $resolver->resolve($config);
+        $this->skipDetection = $options['skip_detection'];
+        $this->sizeLimit = $options['size_limit'];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function handleRequest(RequestInterface $request, callable $next)
+    {
+        if ($this->skipDetection || $request->hasHeader('Content-Type')) {
+            return $next($request);
+        }
+
+        $stream = $request->getBody();
+        $streamSize = $stream->getSize();
+
+        if (empty($streamSize) || $streamSize >= $this->sizeLimit || !$stream->isSeekable()) {
+            return $next($request);
+        }
+
+        if ($this->isJson($stream)) {
+            $request = $request->withHeader('Content-Type', 'application/json');
+
+            return $next($request);
+        } elseif ($this->isXml($stream)) {
+            $request = $request->withHeader('Content-Type', 'application/xml');
+
+            return $next($request);
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function handleResponse(RequestInterface $request, ResponseInterface $response, callable $next)
+    {
+        return $next($request, $response);
+    }
+
+    /**
+     * @param $stream StreamInterface
+     *
+     * @return bool
+     */
+    private function isJson($stream)
+    {
+        $stream->rewind();
+        json_decode($stream->getContents());
+
+        return JSON_ERROR_NONE === json_last_error();
+    }
+
+    /**
+     * @param $stream StreamInterface
+     *
+     * @return \SimpleXMLElement|false
+     */
+    private function isXml($stream)
+    {
+        $stream->rewind();
+        $previousValue = libxml_use_internal_errors(true);
+        $isXml = simplexml_load_string($stream->getContents());
+        libxml_use_internal_errors($previousValue);
+
+        return $isXml;
+    }
+}
---
+++ tests/Unit/Middleware/ContentTypeMiddlewareTest.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Buzz\Test\Unit\Middleware;
+
+use Buzz\Middleware\ContentTypeMiddleware;
+use Nyholm\Psr7\Request;
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\RequestInterface;
+
+class ContentTypeMiddlewareTest extends TestCase
+{
+    public function testMiddleware()
+    {
+        // XML
+        $request = new Request('GET',
+                                'http://foo.com',
+                                [],
+                                '<?xml version="1.0" encoding="UTF-8"?>
+                                <note>
+                                  <to>Lorem</to>
+                                  <from>Ipsum</from>
+                                  <heading>Lorem</heading>
+                                  <body>Ipsum</body>
+                                </note>');
+
+        /** @var RequestInterface $updatedRequest */
+        $updatedRequest = null;
+        $middleware = new ContentTypeMiddleware();
+        $middleware->handleRequest($request, function (RequestInterface $request) use (&$updatedRequest) {
+            $updatedRequest = $request;
+        });
+
+        $this->assertEquals('application/xml', $updatedRequest->getHeaderLine('Content-Type'));
+
+        //JSON
+        $request = new Request('GET',
+                                'http://foo.com',
+                                [],
+                                '{
+                                  "userId": 1,
+                                  "id": 1,
+                                  "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+                                  "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+                                }');
+
+        /** @var RequestInterface $updatedRequest */
+        $updatedRequest = null;
+        $middleware = new ContentTypeMiddleware();
+        $middleware->handleRequest($request, function (RequestInterface $request) use (&$updatedRequest) {
+            $updatedRequest = $request;
+        });
+
+        $this->assertEquals('application/json', $updatedRequest->getHeaderLine('Content-Type'));
+    }
+}
