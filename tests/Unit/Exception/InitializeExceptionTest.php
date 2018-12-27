fd33ab9703734a06ae497b0058c3bac458b4fc21
Nyholm <tobias.nyholm@gmail.com>
2018-12-28 00:45:38 +0100

    minor

--- tests/Unit/Exception/InitializeExceptionTest.php
+++ tests/Unit/Exception/InitializeExceptionTest.php
@@ -4,12 +4,8 @@ declare(strict_types=1);
 
 namespace Buzz\Test\Unit\Exception;
 
-use Buzz\Exception\ClientException;
-use Buzz\Exception\ExceptionInterface;
-use Buzz\Exception\InvalidArgumentException;
-use Buzz\Exception\LogicException;
-use Buzz\Exception\NetworkException;
-use Buzz\Exception\RequestException;
+use Buzz\Exception as BuzzException;
+
 use Nyholm\Psr7\Request;
 use PHPUnit\Framework\TestCase;
 
@@ -17,14 +13,14 @@ class InitializeExceptionTest extends TestCase
 {
     public function testInitialize()
     {
-        $e[] = new ClientException();
-        $e[] = new InvalidArgumentException();
-        $e[] = new LogicException();
-        $e[] = new NetworkException(new Request('GET', '/'));
-        $e[] = new RequestException(new Request('GET', '/'));
+        $e[] = new BuzzException\ClientException();
+        $e[] = new BuzzException\InvalidArgumentException();
+        $e[] = new BuzzException\LogicException();
+        $e[] = new BuzzException\NetworkException(new Request('GET', '/'));
+        $e[] = new BuzzException\RequestException(new Request('GET', '/'));
 
         foreach ($e as $exception) {
-            $this->assertInstanceOf(ExceptionInterface::class, $exception);
+            $this->assertInstanceOf(BuzzException\ExceptionInterface::class, $exception);
         }
     }
 }
