6c9268c52fc7c554037e104be7ef9484822bc2ad
Tobias Nyholm <tobias.nyholm@gmail.com>
2019-02-02 15:01:46 +0100

    Adding WsseAuthMiddleware

---
+++ tests/Unit/Middleware/WsseAuthMiddlewareTest.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Buzz\Test\Unit\Middleware;
+
+use Buzz\Middleware\BasicAuthMiddleware;
+use Buzz\Middleware\WsseAuthMiddleware;
+use Nyholm\Psr7\Request;
+use PHPUnit\Framework\TestCase;
+
+class WsseAuthMiddlewareTest extends TestCase
+{
+    public function testBasicAuthHeader()
+    {
+        $request = new Request('GET', '/');
+
+        $middleware = new WsseAuthMiddleware('foo', 'bar');
+        $newRequest = null;
+        $middleware->handleRequest($request, function ($request) use (&$newRequest) {
+            $newRequest = $request;
+        });
+
+        $this->assertEquals('WSSE profile="UsernameToken"', $newRequest->getHeaderLine('Authorization'));
+        $wsse = $newRequest->getHeaderLine('X-WSSE');
+        $this->assertRegExp('|UsernameToken Username="foo", PasswordDigest=".+?", Nonce=".+?", Created=".+?"|', $wsse);
+    }
+}
