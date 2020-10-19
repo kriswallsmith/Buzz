399657054321ac0e3e7822176628ae247ff5df1f
Chris Doehring <info@chrisdoehring.de>
2020-10-19 23:04:06 +0200

    chore: fix build

--- tests/Unit/Middleware/WsseAuthMiddlewareTest.php
+++ tests/Unit/Middleware/WsseAuthMiddlewareTest.php
@@ -22,7 +22,13 @@ class WsseAuthMiddlewareTest extends TestCase
 
         $this->assertEquals('WSSE profile="UsernameToken"', $newRequest->getHeaderLine('Authorization'));
         $wsse = $newRequest->getHeaderLine('X-WSSE');
-        $method = method_exists($this, 'assertRegExp') ? 'assertRegExp' : 'assertMatchesRegularExpression';
-        $this->$method('|UsernameToken Username="foo", PasswordDigest=".+?", Nonce=".+?", Created=".+?"|', $wsse);
+        $pattern = '|UsernameToken Username="foo", PasswordDigest=".+?", Nonce=".+?", Created=".+?"|';
+        if (method_exists($this, 'assertMatchesRegularExpression')) {
+            $this->assertMatchesRegularExpression($pattern, $wsse);
+
+            return;
+        }
+        // phpunit 7 compatibility
+        $this->assertRegExp($pattern, $wsse);
     }
 }
